f905c06345577f0111f8a6b37e3b97edc18de143
imposter361 <alimir361@gmail.com>
2023-04-21 21:47:11 +0330

    steam link fixer added

--- discordbot.py
+++ discordbot.py
@@ -6,21 +6,33 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client(intents=discord.Intents.default())
+int = discord.Intents.default()
+int.message_content = True
+
+client = discord.Client(intents = int)
 
 @client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
-    )
+    print(f"{username} said: '{user_message}' in channel: ({channel})")
+    
+    steam_store = "https://store.steampowered.com"
+    steam_community = "https://steamcommunity.com"
+    if user_message.startswith(steam_store) or user_message.startswith(steam_community):
+        try:
+            await message.delete()
+            await message.channel.send(f"{message.author.mention}sent a steam link. \nOpen in browser: \
+                                       \n{user_message} \nOpen in Steam: \nsteam://openurl/{user_message}")
+            
+        except Exception as e:
+            print(e)
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
 
