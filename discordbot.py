2e0357e4ca060abd3e92a442f724bde241345f59
imposter361 <alimir361@gmail.com>
2023-04-22 05:52:36 +0330

    minor updates

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,10 @@
 import os
 import discord
 from dotenv import load_dotenv
+from urlextract import URLExtract
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+TOKEN = os.getenv('TESTBOT_TOKEN')
 
 int = discord.Intents.default()
 int.message_content = True
@@ -24,16 +24,20 @@ async def on_message(message):
     
     steam_store = "https://store.steampowered.com"
     steam_community = "https://steamcommunity.com"
-    if user_message.startswith(steam_store) or user_message.startswith(steam_community):
+
+    extractor = URLExtract()
+    UM = extractor.find_urls(f"{user_message}")
+    URL = ''.join(UM)
+    URL = URL.replace("'", "")
+
+    if steam_store in user_message or steam_community in user_message:
         try:
             await message.delete()
-            await message.channel.send(f"{message.author.mention}sent a steam link. \nOpen in browser: \
-                                       \n{user_message} \nOpen in Steam: \nsteam://openurl/{user_message}")
-            
+            await message.channel.send(f"{message.author.mention}sent a steam link and said: \
+                                       \n{user_message} \n\n<:steam:1099147813381746739> Open in Steam: \nsteam://openurl/{URL}")
         except Exception as e:
             print(e)
-
-
+  
 client.run(TOKEN)
 
 
