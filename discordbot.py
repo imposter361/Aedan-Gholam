7acc8aa3aa10a4024b1aa823815a8ac408de7430
imposter361 <130187109+imposter361@users.noreply.github.com>
2023-04-22 06:45:26 +0330

    minor updates

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from urlextract import URLExtract
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('TESTBOT_TOKEN')
 
 int = discord.Intents.default()
 int.message_content = True
@@ -29,17 +29,22 @@ async def on_message(message):
     UM = extractor.find_urls(f"{user_message}")
     URL = ''.join(UM)
     URL = URL.replace("'", "")
+    
+    if user_message.startswith(steam_store) or user_message.startswith(steam_community):
+        try:
+            await message.delete()
+            await message.channel.send(f"{message.author.mention}sent a steam link. \nOpen in browser: \
+                                       \n{user_message} \n\n<:steam:1099147813381746739> Open in Steam: \nsteam://openurl/{user_message}")
+            
+        except Exception as e:
+            print(e)
 
-    if steam_store in user_message or steam_community in user_message:
+    elif steam_store in user_message or steam_community in user_message:
         try:
             await message.delete()
             await message.channel.send(f"{message.author.mention}sent a steam link and said: \
-                                       \n{user_message} \n\n<:steam:1099147813381746739> Open in Steam directly: \nsteam://openurl/{URL}")
+                                       \n{user_message} \n\n<:steam:1099147813381746739> Open in Steam: \nsteam://openurl/{URL}")
         except Exception as e:
             print(e)
   
 client.run(TOKEN)
-
-
-
-
