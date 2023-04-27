1cfda5d9278b8e2294fbb46307ec31c28e0d761b
Sina <120771506+TrustInPy@users.noreply.github.com>
2023-04-27 14:29:22 +0330

    Update discordbot.py
    EPIC_CHANNEL int fixed
    Epic Games emoji added to message

--- discordbot.py
+++ discordbot.py
@@ -95,7 +95,7 @@ async def on_member_remove(member):
 @tasks.loop(hours=12)  
 async def check_discounts():
     await client.wait_until_ready()
-    channel = client.get_channel(EPIC_CHANNEL)
+    channel = client.get_channel(int(EPIC_CHANNEL))
     # Make a request to the Epic Games
     response = requests.get(f'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=US&allowCountries=US&' \
                             f'spaceId=1af6c7f8a3624b1788eaf23175fdd16f&' \
@@ -116,8 +116,8 @@ async def check_discounts():
                 sent_games = [line.strip() for line in file.readlines()]
                 file.close()
             if game_name not in sent_games:
-                message = "The following games are currently available for free on the Epic Games Store:\n"
-                await channel.send(f"<@&1101090907752771595>\n {message}\n* **{game_name}** - (ends {end_date_str})\n{game_link}\n")
+                message = "The following game is currently available for free on the Epic Games Store:"
+                await channel.send(f"<@&1101090907752771595>\n {message}\n<:epic_icon:1101097658153713774> **{game_name}** - (ends {end_date_str})\n{game_link}\n")
                 with open(GAMES_FILE, "a") as file:
                     file.write(game_name + "\n")
 
