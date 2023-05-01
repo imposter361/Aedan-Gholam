047446c65f5fcd5b808970f8be05ee9b40656488
imposter361 <alimir361@gmail.com>
2023-05-01 08:21:13 +0330

    minore updates

--- discordbot.py
+++ discordbot.py
@@ -126,13 +126,14 @@ async def check_discounts():
                 print(e) # log
 
 # send Klei point links in a channel.
-@tasks.loop(hours=12)
+@tasks.loop(seconds=20)
 async def dst():
     # specify the URL of the web page
     url = 'https://steamcommunity.com/sharedfiles/filedetails/?id=2308653652&tscn=1639750749'
     channel = client.get_channel(int(EPIC_CHANNEL))
     # send a GET request to the URL
     response = requests.get(url)
+
     # parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
     link_selector = 'a.bb_link[href*="https://accounts.klei.com/link/"]'
@@ -148,7 +149,7 @@ async def dst():
                     sent_link = [line.strip() for line in file.readlines()]
                     file.close()
                 if link not in sent_link:
-                    await channel.send(f"<@&1101266966771155015>\nopen this <{link}>")
+                    await channel.send(f"<@&1101266966771155015>\n<:dst_icon:1101262983788769351> open this link to claim **klei point** for **Don't starve together**:\n<{link}>")
                     with open(KLEI_LINKS, "a") as file:
                         file.write(link + "\n")
         except Exception as e:
