02b53181a337d755cc0fa45b05f018cd006aefe7
TrustInPy <sinasolimany@gmail.com>
2023-05-08 21:29:40 +0330

    Member count message edited

--- MemberCount.py
+++ MemberCount.py
@@ -8,8 +8,8 @@ async def member_count():
     members_count_channel = client.get_channel(int(MEMBER_COUNT_CH_ID))
     name = "Total members: " + str(members_count_channel.guild.member_count)
     await members_count_channel.edit(name=name)
-    print(f'Total members is now {name}')
-    logging.info(f'Total members is now {name}')
+    print(f'« {members_count_channel.guild.member_count} » people are in the house')
+    logging.info(f'« {members_count_channel.guild.member_count} » people are in the house')
 
 
 def setup_MemberCount(bot):
